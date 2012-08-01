3ef058394574c630726c955ba2a54a63df453d08
Sem <sembrestels@riseup.net>
2012-08-01 21:13:28 +0200

    Fixed image full view lightbox.

--- views/default/js/photos/tidypics.php
+++ views/default/js/photos/tidypics.php
@@ -12,8 +12,12 @@ elgg.tidypics.init = function() {
 	if (elgg.ui.lightbox) {
 		$('.elgg-lightbox, .elgg-lightbox-photo').colorbox({
 			href: function() {
-				var guid = (new RegExp("photos/image/[0-9]+", 'i')).exec($(this).attr('href')).toString().substr("photos/image/".length);
-				return elgg.config.wwwroot + "photos/thumbnail/" + guid + "/large";
+				if ((new RegExp("photos/image/[0-9]+", 'i')).test($(this).attr('href'))) {
+					var guid = (new RegExp("photos/image/[0-9]+", 'i')).exec($(this).attr('href')).toString().substr("photos/image/".length);
+					return elgg.config.wwwroot + "photos/thumbnail/" + guid + "/large";
+				} else {
+					return $(this).attr('href');
+				}
 			},
 			title: function() {
 				return '<h3 style="display: inline">'+ $(this).find('img').attr('title') +'</h3> - <a href="'+ $(this).attr('href') +'">'+ elgg.echo('comments') +'</a>';
